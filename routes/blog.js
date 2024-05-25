bf7e04ad364e474f1f29c22c468770e6b3e11740
mukilan <mukil.acumen@gmail.com>
2024-05-25 14:11:19 +0530

    mongodb completed

--- routes/blog.js
+++ routes/blog.js
@@ -27,16 +27,57 @@ router.get('/posts/:id', async function (req, res) {
   if (!post) {
     return res.status(404).render('404');
   }
+  post.humanReadableDate = post.date.toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+  post.date = post.date.toISOString()
   res.render('post-detail', { post: post })
 })
 
+router.get('/posts/:id/edit', async function (req, res) {
+  const postid = req.params.id;
+  const post = await db
+    .checkconnection()
+    .collection('posts')
+    .findOne({ _id: new ObjectId(postid) },
+      { title: 1, summary: 1, body: 1 });
+  if (!post) {
+    return res.status(404).render('404');
+  }
+  res.render('update-post', { post: post })
+})
 
-
+router.post('/posts/:id/edit', async function (req, res) {
+  const postid = new ObjectId(req.params.id);
+  const updatepostdata = await db
+    .checkconnection()
+    .collection('posts')
+    .updateOne({ _id: postid }, {
+      $set: {
+        title: req.body.title,
+        summary: req.body.summary,
+        body: req.body.content
+      }
+    });
+  res.redirect('/posts')
+})
+router.post('/posts/:id/delete', async function (req, res) {
+  const deletePost = new ObjectId(req.params.id);
+  const deletePosts = await db
+    .checkconnection()
+    .collection('posts')
+    .deleteOne({ _id: deletePost })
+res.redirect('/posts')
+})
 router.get('/new-post', async function (req, res) {
   const authors = await db.checkconnection().collection('authors').find().toArray();
 
   res.render('create-post', { author: authors });
 });
+
 router.post('/posts', async function (req, res) {
   const authorid = new ObjectId(req.body.author);
   const author = await db.checkconnection().collection('authors').findOne({ _id: authorid })
