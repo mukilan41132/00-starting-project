601def411b8fe3a8923b5a096dd996b4b34f5180
mukil <you@example.com>
2024-06-30 09:55:49 +0530

    d

--- Data/MongodbConnection.js
+++ Data/MongodbConnection.js
@@ -5,12 +5,12 @@ let database;
 
 async function connect() {
     const client = await MongoClient.connect('mongodb://127.0.0.1:27017/');
-    database = client.db('blog');
+    database = client.db('authors');
 };
 
 function dbconnectionerror() {
     if (!database) {
-        throw { message: 'database is not established!' };
+        alert('database is not established!');
     }
     return database;
 }
